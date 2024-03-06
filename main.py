754a096ea996e5c329ba13b36938188a373993c0
soumyap22 <pandeysoumya22@gmail.com>
2024-03-07 00:59:42 +0530

    some changes done

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ app = Flask(__name__ , template_folder='templates')
 
 @app.route('/')
 def index():
-    # return redirect(url_for("index"))
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 @app.route('/achievements')
 def achievements():
@@ -22,15 +17,6 @@ def contact():
 @app.route('/skills')
 def skills():
     return render_template('skills_projects.html')
-
-@app.route('/photos')
-def photos():
-    return render_template('photos.html')
-
-@app.route('/learn_more')
-def learn_more():
-    return render_template('learn_more.html')
-
-    
+   
 if __name__ == '__main__':
     app.run(host='0.0.0.0')    
